f14bc816beeac6af213a8fab162a77b50e3f4087
midhunpaini <chatikko.official@gmail.com>
2026-02-09 23:06:32 +0530

    hash routing added

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createHashRouter, Navigate } from "react-router-dom";
 import { AppShell } from "@/components/layout/AppShell";
 import { ProtectedRoute } from "@/components/routing/ProtectedRoute";
 import { AdminRoute } from "@/components/routing/AdminRoute";
@@ -20,7 +20,7 @@ import { AdminContractsPage } from "@/pages/admin/AdminContractsPage";
 import { AdminAuthEventsPage } from "@/pages/admin/AdminAuthEventsPage";
 import { AdminBillingEventsPage } from "@/pages/admin/AdminBillingEventsPage";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   { path: "/", element: <LandingPage />, errorElement: <ErrorPage /> },
   { path: "/login", element: <LoginPage />, errorElement: <ErrorPage /> },
   { path: "/signup", element: <SignupPage />, errorElement: <ErrorPage /> },
